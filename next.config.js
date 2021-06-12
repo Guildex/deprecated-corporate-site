f9adb428e5f123869d4bc4dfd59b98d628066396
yuyake0084 <yuyake0084@gmail.com>
2021-06-12 16:53:18 +0900

    :recycle: next exportするように修正

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ module.exports = ({
   },
   exportPathMap: true,
   pageExtensions: ["tsx"],
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
   webpack: (config) => ({
     ...config,
     module: {
