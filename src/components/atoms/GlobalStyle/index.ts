3169d64405aeeb625cb6b16437d71d2911b84288
yuyake0084 <yuyake0084@gmail.com>
2021-06-15 09:42:34 +0900

    :bug:  画像が縦に表示される不具合の修正

--- src/components/atoms/GlobalStyle/index.ts
+++ src/components/atoms/GlobalStyle/index.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   html,
   body {
     line-height: 1.5;
+    font-family: 'YuGothic', '游ゴシック体';
     background-color: #f1ebde;
   }
   
@@ -17,38 +18,38 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   input,
-button,
-select,
-textarea {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  background: transparent;
-  border: none;
-  border-radius: 0;
-  font: inherit;
-  outline: none;
-}
+  button,
+  select,
+  textarea {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background: transparent;
+    border: none;
+    border-radius: 0;
+    font: inherit;
+    outline: none;
+  }
 
-textarea {
-  height: 200px;
-  resize: vertical;
-}
+  textarea {
+    height: 200px;
+    resize: vertical;
+  }
 
-input[type='checkbox'],
-input[type='radio'] {
-  display: none;
-}
+  input[type='checkbox'],
+  input[type='radio'] {
+    display: none;
+  }
 
-input[type='submit'],
-input[type='button'],
-label,
-button,
-select {
-  cursor: pointer;
-}
+  input[type='submit'],
+  input[type='button'],
+  label,
+  button,
+  select {
+    cursor: pointer;
+  }
 
-select::-ms-expand {
-  display: none;
-}
+  select::-ms-expand {
+    display: none;
+  }
 `
