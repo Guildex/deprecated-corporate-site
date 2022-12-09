6741e383e296eef71fcefa349c7559cf927cf540
kimuniiii <kk.baseball.0824@gmail.com>
2022-12-09 10:17:59 +0900

    test : Preview Deploy できるかどうかテストする

--- src/components/atoms/CompanyProfile/CompanyProfile.tsx
+++ src/components/atoms/CompanyProfile/CompanyProfile.tsx
@@ -62,6 +62,7 @@ export const CompanyProfile: React.FC = () => {
           </Record>
         ))}
       </Inner>
+
       <GoogleMapBox>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.6732088147423!2d139.7345331160028!3d35.685047780193464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c63637ee9ff%3A0x8cc0e2d1261eb30!2zVEhFIEJBU0Ug6bq555S6IOOCouOCu-ODg-ODiOODh-OCtuOCpOODsw!5e0!3m2!1sja!2sjp!4v1623131130297!5m2!1sja!2sjp"
