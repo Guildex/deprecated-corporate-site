cd8e16a2c792ba5ade81ec7c64e7f8354c24d27e
Kimura Kazuki <59758368+kimuniiii@users.noreply.github.com>
2023-02-21 12:18:39 +0900

    住所を「渋谷スクランブルスクエア」に変更 and 電話番号削除 and Google マップのピンの位置を渋谷スクランブルスクエアに変更 (#3)
    * 住所をスクランブルスクエアに変更
    
    * feature : 渋谷を追加
    
    * 電話番号削除 and 住所変更
    
    * const.ts ファイルに定数をまとめる
    
    * 定数ファイルを import する
    
    * コメントを追加する

---
+++ src/components/atoms/CompanyProfile/const.ts
@@ -0,0 +1,36 @@
+import { formatMoneyJapaneseYen } from "@/utils/format-money-japanese-yen";
+
+/**
+ * ### API KEY なしでサイトにGoogleマップを表示させるための埋め込みコード
+ * - 以下の定数を iframe の src属性 に指定することで Googleマップ を表示できる
+ * @see https://www.webdesignleaves.com/pr/plugins/googlemap_02.html
+ */
+export const GOOGLE_MAP_ADDRESS =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1620.876356098532!2d139.70120503573096!3d35.65846379538141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b8427e1c0b1%3A0x78f6e23397061d6f!2z5riL6LC344K544Kv44Op44Oz44OW44Or44K544Kv44Ko44Ki!5e0!3m2!1sja!2sjp!4v1676948522584!5m2!1sja!2sjp";
+
+export const COMPANY_DATA = [
+  {
+    key: "会社名",
+    value: "合同会社Guildex",
+  },
+  {
+    key: "資本金",
+    value: `${formatMoneyJapaneseYen(2100000)}円`,
+  },
+  {
+    key: "設立",
+    value: "2021年5月25日",
+  },
+  {
+    key: "最高経営責任者",
+    value: "大橋 勇",
+  },
+  {
+    key: "事業内容",
+    value: "「Bmake（ビーメイク）」企画・開発・運営\nWebアプリケーション及びWebシステム受託開発",
+  },
+  {
+    key: "住所",
+    value: "〒150-0002 東京都渋谷区渋谷2-24-12 渋谷スクランブルスクエア39F",
+  },
+];
